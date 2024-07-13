6a607a610cec13875310dc7a0be4b7c3879b2629
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-07-13 18:42:14 -0300

    Implementação das rotas de links

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { confirmTrip } from "./routes/confirm-trip";
 import { createActivity } from "./routes/create-activity";
 import { createTrip } from "./routes/create-trip";
 import { getActivities } from "./routes/get-ativities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -22,6 +24,8 @@ app.register(confirmTrip);
 app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
